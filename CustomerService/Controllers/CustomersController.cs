092a935902d7e029b79860016c3d333eb327070e
Restuatmiko <restuaprianto15@gmail.com>
2026-09-01 23:30:45 +0700

    feat: setup customer service and database schema

---
+++ CustomerService/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using CustomerService.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var customers = await _context.Customers
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(customers);
+        }
+    }
+}
---
+++ CustomerService/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerService.Models
+{
+    [Table("COM_CUSTOMER")]
+    public class Customer
+    {
+        [Key]
+        [Column("COM_CUSTOMER_ID")]
+        public int COM_CUSTOMER_ID { get; set; }
+
+        [Column("CUSTOMER_NAME")]
+        public string CUSTOMER_NAME { get; set; } = string.Empty;
+    }
+}
